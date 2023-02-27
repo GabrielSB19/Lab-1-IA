65a3005fea40011321e841ba874749a8791e3c1a
GabrielSB19 <gasuarez2002@gmail.com>
2023-02-26 19:37:32 -0500

    Startin the project - Module 1

---
+++ methods.py
@@ -0,0 +1,8 @@
+def exponente (lista, exp):
+    list = []
+    for i in lista:
+        list.append(i**exp)
+    return list
+
+def perimetroRectangulo (base, altura):
+    return 2*base + 2*altura
